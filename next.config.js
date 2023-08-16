1325b3da3cf5822899ed6be965fbf27f7186a2f1
LikithKumarDK41 <99169124+LikithKumarDK41@users.noreply.github.com>
2023-08-16 18:43:07 +0530

    Update: Next config

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
     compiler: { styledComponents: true, },
     reactStrictMode: true,
     trailingSlash: false,
-    // output: 'export', // To enable a static export, change the output mode !!
+    output: 'export', // To enable a static export, change the output mode !!
     env: {
         customKey: process.env.NODE_ENV === 'development' ? 'my-value-dev' : 'my-value-prod',
     },
