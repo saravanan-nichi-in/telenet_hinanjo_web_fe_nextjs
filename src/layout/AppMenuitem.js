134e00a1c1ae15db4019083633288942de73fd46
LikithKumarDK41 <99169124+LikithKumarDK41@users.noreply.github.com>
2023-09-21 16:10:23 +0530

    Update: UI/UX / #1992

--- src/layout/AppMenuitem.js
+++ src/layout/AppMenuitem.js
@@ -61,7 +61,7 @@ const AppMenuitem = (props) => {
 
     return (
         <li className={classNames({ 'layout-root-menuitem': props.root, 'active-menuitem': active })}>
-            {props.root && item.visible !== false && <div className="layout-menuitem-root-text">{item.label}</div>}
+            {/* {props.root && item.visible !== false && <div className="layout-menuitem-root-text">{item.label}</div>} */}
             {(!item.to || item.items) && item.visible !== false ? (
                 <a href={item.url} onClick={(e) => itemClick(e)} className={classNames(item.class, 'p-ripple')} target={item.target} tabIndex="0">
                     <span className={classNames('layout-menuitem-icon', item.icon)}>
