175875b2bab86a59890f8eac945e6f0de6a85956
narayanasamy-angamuthu <angamuthu.n@nichi.com>
2023-10-03 12:48:34 +0530

    Created External Evacuee Screen

--- src/pages/admin/evacuation/family-detail/index.js
+++ src/pages/admin/evacuation/family-detail/index.js
@@ -1,30 +1,40 @@
 import React, { useState, useContext, useEffect } from 'react';
-import { useRouter } from 'next/router'
 
 import { getValueByKeyRecursively as translate } from '@/helper'
 import { LayoutContext } from '@/layout/context/layoutcontext';
-import { Button, NormalTable, RowExpansionTable } from '@/components';
-import { evacueeFamilyDetailColumns, familyDetailColumns, familyDetailData, evacueeFamilyDetailRowExpansionColumns, familyDetailData1, familyDetailColumns1, townAssociationData, townAssociationColumn } from '@/utils/constant';
+import { NormalTable, RowExpansionTable } from '@/components';
+import { evacueeFamilyDetailColumns, familyDetailColumns, familyDetailData } from '@/utils/constant';
 import { AdminEvacueeFamilyDetailService } from '@/helper/adminEvacueeFamilyDetailService';
 
 export default function EvacueeFamilyDetail() {
     const { localeJson } = useContext(LayoutContext);
     const [admins, setAdmins] = useState([]);
-    const router = useRouter();
 
     useEffect(() => {
-        AdminEvacueeFamilyDetailService.getEvacueeFamilyDetailWithOrdersSmall().then((data) => setAdmins(data));
+        AdminEvacueeFamilyDetailService.getAdminsEvacueeFamilyDetailMedium().then((data) => setAdmins(data));
     }, []);
+
+    const rowExpansionTemplates = (data) => {
+        return (
+            <div className="orders-subtable">
+                <h5>Orders for Data</h5>
+            </div>
+        );
+    }
 
     return (
         <div className="grid">
             <div className="col-12">
                 <div className='card'>
-                    <h5 className='page-header1'>{translate(localeJson, 'house_hold_information_details')}</h5>
+                    <h5 className='page_header'>{translate(localeJson, 'house_hold_information_details')}</h5>
                     <hr />
-                    <div className='mb-2'>
-                        <div className='flex justify-content-end' style={{ textDecoration: "underline" }}>
-                            {translate(localeJson, 'household_number')} 001-001
+                    <div>
+                        <div>
+                            <form>
+                                <div className='mt-5 mb-3 flex flex-wrap align-items-center justify-content-end gap-2 mobile-input'>
+
+                                </div>
+                            </form>
                         </div>
                     </div>
                     <NormalTable
@@ -35,54 +45,30 @@ export default function EvacueeFamilyDetail() {
                         showGridlines={true}
                         value={familyDetailData}
                         columns={familyDetailColumns}
-                        parentClass="mb-2"
+                        parentClass="pb-2"
                     />
-                    <div className='mb-2'>
-                        <h5 className='page-header2'>{translate(localeJson, 'household_list')}</h5>
+                    <div className='pb-2'>
+                        <h5>{translate(localeJson, 'household_list')}</h5>
                     </div>
                     <RowExpansionTable
-                        rows={10}
-                        paginatorLeft={true}
-                        paginator="true"
-                        customRowExpansionActionsField="actions"
+                        id="evacuee-family-detail"
+                        size={"small"}
+                        rows={10} 
+                        rowExpansionColumnStyle={{ textAlign: 'center' }}
+                        columnStyle={{ textAlign: 'center' }} 
+                        paginator={true} 
+                        showGridlines={true}
+                        customRowExpansionActionsField="actions" 
                         value={admins}
-                        innerColumn={evacueeFamilyDetailRowExpansionColumns}
                         outerColumn={evacueeFamilyDetailColumns}
                         rowExpansionField="orders"
-                    />
-                    <div className='mt-2 flex justify-content-center overflow-x-auto'>
-                        <NormalTable
-                            id="evacuee-family-detail"
-                            size={"small"}
-                            stripedRows={true}
-                            paginator={false}
-                            showGridlines={true}
-                            tableStyle={{ maxWidth: "10rem" }}
-                            value={townAssociationData}
-                            columns={townAssociationColumn}
+                        emptyMessage="No Data found."
+                        rowExpansionTemplate={rowExpansionTemplates}
+                        paginatorLeft={true}
+                        expander={true}
+                        onRowToggle={(e) => setExpandedRows(e.data)}
+                        expandAllButtonProps={false}
                         />
-                    </div>
-                    <div className='mt-2 flex justify-content-center overflow-x-auto'>
-                        <NormalTable
-                            id="evacuee-family-detail"
-                            size={"small"}
-                            stripedRows={true}
-                            paginator={false}
-                            showGridlines={true}
-                            tableStyle={{ maxWidth: "20rem" }}
-                            value={familyDetailData1}
-                            columns={familyDetailColumns1}
-                        />
-                    </div>
-                    <div className="text-center mt-2">
-                        <Button buttonProps={{
-                            buttonClass: "text-600 w-8rem",
-                            bg: "bg-white",
-                            hoverBg: "hover:surface-500 hover:text-white",
-                            text: translate(localeJson, 'return'),
-                            onClick: () => router.push('/admin/evacuation/'),
-                        }} parentClass={"inline"} />
-                    </div>
                 </div>
             </div>
         </div>
--- src/pages/admin/external/family/index.js
+++ src/pages/admin/external/family/index.js
@@ -1,9 +1,152 @@
-import React from 'react'
+import React, { useState, useContext, useEffect } from 'react';
+import { Chart } from 'primereact/chart';
 
-export default function index() {
+import { LayoutContext } from '@/layout/context/layoutcontext';
+import { getValueByKeyRecursively as translate } from '@/helper'
+import { Button } from '@/components';
+
+
+export default function ExternalEvacuees() {
+    const { localeJson } = useContext(LayoutContext);
+    const [chartData, setChartData] = useState({});
+    const [chartOptions, setChartOptions] = useState({});
+    const [pieChartData, setPieChartData] = useState({});
+    const [pieChartOptions, setPieChartOptions] = useState({});
+    const [pieChartQuestionData, setPieChartQuestionData] = useState({});
+    const [pieChartQuestionOptions, setPieChartQuestionOptions] = useState({});
+
+    useEffect(() => {
+        const data = {
+            datasets: [
+                {
+                    label: 'Vacant test',
+                    data: [32, 1, 1, 1],
+                    backgroundColor: [
+                        'rgba(255, 159, 64, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(153, 102, 255, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgb(255, 159, 64)',
+                        'rgb(75, 192, 192)',
+                        'rgb(54, 162, 235)',
+                        'rgb(153, 102, 255)'
+
+                    ],
+                    borderWidth: 1,
+                }
+            ],
+
+        };
+
+        const options = {
+            scales: {
+                y: {
+                    beginAtZero: true,
+                    label: "人数"
+                },
+                x: {
+                    label: "避難所"
+                }
+            },
+            plugins: {
+                legend: {
+                    labels: {
+                        usePointStyle: true
+                    }
+                }
+            }
+        };
+        const documentStyle = getComputedStyle(document.documentElement);
+        const pieChartData = {
+            labels: ['市内', '市外', '県外'],
+            datasets: [
+                {
+                    data: [29, 2, 2],
+                    backgroundColor: [
+                        documentStyle.getPropertyValue('--blue-500'),
+                        documentStyle.getPropertyValue('--yellow-500'),
+                        documentStyle.getPropertyValue('--green-500')
+                    ],
+                    hoverBackgroundColor: [
+                        documentStyle.getPropertyValue('--blue-400'),
+                        documentStyle.getPropertyValue('--yellow-400'),
+                        documentStyle.getPropertyValue('--green-400')
+                    ]
+                }
+            ]
+        }
+        const pieChartOptions = {
+            plugins: {
+                legend: {
+                    labels: {
+                        usePointStyle: true
+                    }
+                }
+            }
+        };
+        const pieChartQuestionData = {
+            labels: ['はい', 'いいえ'],
+            datasets: [
+                {
+                    data: [35, 2],
+                    backgroundColor: [
+                        documentStyle.getPropertyValue('--blue-500'),
+                        documentStyle.getPropertyValue('--yellow-500'),
+                        documentStyle.getPropertyValue('--green-500')
+                    ],
+                    hoverBackgroundColor: [
+                        documentStyle.getPropertyValue('--blue-400'),
+                        documentStyle.getPropertyValue('--yellow-400'),
+                        documentStyle.getPropertyValue('--green-400')
+                    ]
+                }
+            ]
+        }
+        const pieChartQuestionOptions = {
+            plugins: {
+                legend: {
+                    labels: {
+                        usePointStyle: true
+                    }
+                }
+            }
+        };
+        setChartData(data);
+        setChartOptions(options);
+        setPieChartData(pieChartData);
+        setPieChartOptions(pieChartOptions);
+        setPieChartQuestionData(pieChartQuestionData);
+        setPieChartQuestionOptions(pieChartQuestionOptions);
+    }, []);
+
     return (
-        <div>
-            External Evacuees Tally
+        <div className="grid">
+            <div className="col-12">
+                <div className='card'>
+                    <div style={{ display: "flex", justifyContent: "space-between", alignItems: 'center' }}>
+                        <h5 className='page-header1'>{translate(localeJson, 'external_evacuees_tally')}</h5>
+                        <span>{translate(localeJson, 'external_evacuees_count') + ": 39人"}</span>
+                    </div>
+                    <hr />
+                    <div className='flex justify-content-end pt-2'>
+                        <Button buttonProps={{
+                            text: translate(localeJson, "external_evacuee_details"),
+                            severity: "primary"
+                        }} />
+                    </div>
+                    <div className='mb-2'>
+                        <Chart type="bar" data={chartData} options={chartOptions} />
+                    </div>
+
+                    <div className='mb-2' style={{ display: "flex", justifyContent: "space-between", alignItems: 'center' }}>
+                        <Chart type="pie" data={pieChartData} options={pieChartOptions} className="w-full md:w-30rem" />
+                        <Chart type="pie" data={pieChartQuestionData} options={pieChartQuestionOptions} className="w-full md:w-30rem" />
+                    </div>
+
+                </div>
+            </div>
         </div>
     )
 }
