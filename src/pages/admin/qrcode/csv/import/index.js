fa535c076eac80f16d5b32686b7ee8f8fee4e1b3
LikithKumarDK41 <99169124+LikithKumarDK41@users.noreply.github.com>
2023-10-11 19:08:46 +0530

    Update: UI/UX / Integration ( #2012 )

--- src/pages/admin/qrcode/csv/import/index.js
+++ src/pages/admin/qrcode/csv/import/index.js
@@ -2,7 +2,6 @@ import React, { useContext, useState, useEffect } from 'react';
 import { useRouter } from 'next/router'
 import { Formik } from 'formik';
 import * as Yup from 'yup';
-import _ from 'lodash';
 
 import { getValueByKeyRecursively as translate } from '@/helper'
 import { LayoutContext } from '@/layout/context/layoutcontext';
