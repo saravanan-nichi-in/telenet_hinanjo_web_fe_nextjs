22f1d4dc0f5d21534337e282d96e6794492d3444
sandeepNIS <sandeep.r@nichi.com>
2023-10-17 18:50:03 +0530

    Update:UI/UX /Settings screen design and import modal bug fix

--- src/components/modal/adminManagementImportModal.js
+++ src/components/modal/adminManagementImportModal.js
@@ -12,8 +12,8 @@ import { InputFile } from "../upload";
 
 export default function AdminManagementImportModal(props) {
     const { localeJson } = useContext(LayoutContext);
-    const { open, close, register,modalHeaderText } = props && props;
-    const router=useRouter();
+    const { open, close, register, modalHeaderText, style} = props && props;
+    const router = useRouter();
     const header = (
         <div className="custom-modal">
             {modalHeaderText}
@@ -22,7 +22,7 @@ export default function AdminManagementImportModal(props) {
     const schema = Yup.object().shape({
         file: Yup.mixed()
             .required(translate(localeJson, 'file_csv_required'))
-            .test('is-csv',translate(localeJson, 'select_csv_format'), (value) => {
+            .test('is-csv', translate(localeJson, 'select_csv_format'), (value) => {
                 if (!value) return true; // If no file is selected, the validation passes.
 
                 const allowedExtensions = ['csv']; // Define the allowed file extensions (in this case, just 'csv').
@@ -40,7 +40,7 @@ export default function AdminManagementImportModal(props) {
                 initialValues={{ file: null }}
                 onSubmit={(values) => {
                     props.importFile(values.file)
-                    values.file=null;
+                    values.file = null;
                 }}
             >
                 {({
@@ -50,16 +50,18 @@ export default function AdminManagementImportModal(props) {
                     setFieldValue,
                     handleSubmit,
                 }) => (
-                    <div>
+                    <div className="">
                         <form onSubmit={handleSubmit}>
                             <Dialog
-                                className="custom-modal"
+                                className={`importModal_width h-20rem lg:h-20rem md:h-20rem sm:h-20rem custom-modal modal-import`}
                                 header={header}
                                 visible={open}
+                                style={style}
                                 draggable={false}
                                 onHide={() => {
                                     close();
-                                    values.file=null}}
+                                    values.file = null
+                                }}
                                 footer={
                                     <div className="text-center">
                                         <Button buttonProps={{
@@ -74,15 +76,16 @@ export default function AdminManagementImportModal(props) {
                                     </div>
                                 }
                             >
-                                <div className={`modal-content`}>
-                                    <InputFile inputFileProps={{
-                                    
-                                        onChange: (event) => {
-                                            setFieldValue("file", event.currentTarget.files[0]);
-                                        },
-                                    }} parentClass={`w-full ${errors.file && touched.file && 'p-invalid '}`} />
-                                    <div className='pt-1'>
-                                        <ValidationError errorBlock={errors.file && touched.file && errors.file} />
+                                <div className="col-12 align-self-center">
+                                    <div className={`modal-content  `}>
+                                        <InputFile inputFileProps={{
+                                            onChange: (event) => {
+                                                setFieldValue("file", event.currentTarget.files[0]);
+                                            },
+                                        }} parentClass={`w-full ${errors.file && touched.file && 'p-invalid '}`} />
+                                        <div className='pt-1'>
+                                            <ValidationError errorBlock={errors.file && touched.file && errors.file} />
+                                        </div>
                                     </div>
                                 </div>
                             </Dialog>
