15c4dac8ef413bb20ae2ac68d7cb54092d5c5931
aakash-gandhi-13 <aakash.gandhi@nichi.com>
2023-10-19 00:48:05 +0530

    staff management list and search api

--- src/pages/admin/staff-management/index.js
+++ src/pages/admin/staff-management/index.js
@@ -6,65 +6,57 @@ import { LayoutContext } from '@/layout/context/layoutcontext';
 import { Button, InputFloatLabel, NormalTable } from '@/components';
 import { StaffDetailService } from '@/helper/StaffDetailService';
 import { AdminManagementDeleteModal, AdminManagementImportModal, StaffManagementDetailModal, StaffManagementEditModal } from '@/components/modal';
+import { StaffManagementService } from '@/services/staffmanagement.service';
 
 export default function StaffManagementPage() {
-    const { localeJson, setLoader } = useContext(LayoutContext);
+    const { localeJson, setLoader, locale } = useContext(LayoutContext);
     const [staff, setStaff] = useState([]);
-    const router = useRouter();
     const [importStaffOpen, setImportStaffOpen] = useState(false);
     const [staffDetailsOpen, setStaffDetailsOpen] = useState(false);
     const [deleteStaffOpen, setDeleteStaffOpen] = useState(false);
     const [editStaffOpen, setEditStaffOpen] = useState(false);
     const [createStaffOpen, setCreateStaffOpen] = useState(false);
-    const staffs = [
-        { field: 'No', header: 'S No', minWidth: "3rem" },
+    const [searchName, setSearchName] = useState("");
+
+    const columnsData = [
+        { field: 'id', header: 'S No', minWidth: "3rem" },
         {
-            field: '氏名', header:translate(localeJson, 'name'), minWidth: "15rem", body: (rowData) => (
+            field: 'name', header:translate(localeJson, 'name'), minWidth: "5rem", body: (rowData) => (
                 <a className='text-decoration' onClick={() => setStaffDetailsOpen(true)}>
-                    {rowData['氏名']}
+                    {rowData['name']}
                 </a>
             )
         },
-        { field: 'メール', header: translate(localeJson, 'address_email') },
-        { field: '電話番号', header:translate(localeJson, 'tel') },
+        { field: 'email', header: translate(localeJson, 'address_email'), minWidth: "5rem" },
+        { field: 'tel', header:translate(localeJson, 'tel') },
         {
             field: 'actions',
-            header: translate(localeJson, 'edit'),
+            header: translate(localeJson, 'common_action'),
             textAlign: "center",
             body: (rowData) => (
                 <div>
-                    <Button buttonProps={{
-                        text: translate(localeJson, 'edit'), buttonClass: "text-primary",
+                    <Button 
+                        parentStyle={{ display: "inline" }}
+                        buttonProps={{
+                        text: translate(localeJson, 'edit'), 
+                        buttonClass: "text-primary",
                         bg: "bg-white",
                         hoverBg: "hover:bg-primary hover:text-white",
                         onClick: () => setEditStaffOpen(true)
                     }} />
-                </div>
-            ),
-        }, {
-            field: 'actions',
-            header: translate(localeJson, 'delete'),
-            textAlign: "center",
-            body: (rowData) => (
-                <div>
-                    <Button buttonProps={{
-                        text: translate(localeJson, 'delete'), buttonClass: "text-primary",
+                    <Button 
+                        parentStyle={{ display: "inline" }}
+                        buttonProps={{
+                        text: translate(localeJson, 'delete'), 
+                        buttonClass: "text-primary ml-2",
                         bg: "bg-red-600 text-white",
                         hoverBg: "hover:bg-red-500 hover:text-white",
                         onClick: () => setDeleteStaffOpen(true)
-                    }} />   
+                    }} />  
                 </div>
             ),
-        },
+        }
     ];
-
-    useEffect(() => {
-        const fetchData = async () => {
-            await StaffDetailService.getStaffMedium().then((data) => setStaff(data));
-            setLoader(false);
-        };
-        fetchData();
-    }, []);
 
     const onStaffImportClose = () => {
         setImportStaffOpen(!importStaffOpen);
@@ -88,6 +80,94 @@ export default function StaffManagementPage() {
         setCreateStaffOpen(false);
     };
 
+
+    // Main Table listing starts
+    const { getList } = StaffManagementService;
+
+    const [getListPayload, setGetListPayload] = useState( {
+        "filters": {
+            "start": 0,
+            "limit": 5,
+            "order_by": "desc",
+            "sort_by": "updated_at"
+        },
+        "name" : ""
+    });
+
+    const [columns, setColumns] = useState([]);
+    const [list, setList] = useState([]);
+    const [totalCount, setTotalCount] = useState(0);
+    const [tableLoading, setTableLoading] = useState(false);
+    
+    const onGetMaterialListOnMounting = () => {
+        // Get dashboard list
+        getList(getListPayload, (response) => {
+            if (response.success && !_.isEmpty(response.data) && response.data.model.total > 0) {
+                const data = response.data.model.list;
+                var additionalColumnsArrayWithOldData = [...columnsData];
+                let preparedList = [];
+                // Update prepared list to the state
+                // Preparing row data for specific column to display
+                console.log(data);
+                data.map((obj, i) => {
+                    let preparedObj = {
+                        id: i + getListPayload.filters.start + 1,
+                        name: obj.name ?? "",
+                        email: obj.email ?? "",
+                        image: obj.image ?? "",
+                        tel: obj.tel ?? "",
+                        birthday: obj.birthday ?? "",
+                        zip_code: obj.zip_code ?? "",
+                        prefecture_id: obj.prefecture_id ?? "",
+                        address: obj.address ?? "",
+                        first_login: obj.first_login ?? "",
+                    }
+                    preparedList.push(preparedObj);
+                })
+                console.log(preparedList);
+                setList(preparedList);
+                setColumns(additionalColumnsArrayWithOldData);
+                setTotalCount(response.data.model.total);
+                setTableLoading(false);
+            } else {
+                setTableLoading(false);
+                setList([]);
+            }
+        });
+
+    }
+
+    /**
+     * Pagination handler
+     * @param {*} e 
+     */
+    const onPaginationChange = async (e) => {
+        setTableLoading(true);
+        if (!_.isEmpty(e)) {
+            const newStartValue = e.first; // Replace with your desired page value
+            const newLimitValue = e.rows; // Replace with your desired limit value
+            await setGetListPayload(prevState => ({
+                ...prevState,
+                filters: {
+                    ...prevState.filters,
+                    start: newStartValue,
+                    limit: newLimitValue
+                }
+            }));
+        }
+    }
+
+    useEffect(() => {
+        setTableLoading(true);
+        const fetchData = async () => {
+            await onGetMaterialListOnMounting()
+            setLoader(false);
+        };
+        fetchData();
+    }, [locale, getListPayload]);
+
+    // Main table listing ends
+    
     return (
         <React.Fragment>
             <AdminManagementImportModal
@@ -147,17 +227,18 @@ export default function StaffManagementPage() {
                                     text: translate(localeJson, 'create_staff'),
                                     onClick: () => setCreateStaffOpen(true),
                                     severity: "success"
-                                }} parentClass={"mr-1 mt-1"} />
+                                }} parentClass={"mt-1"} />
                             </div>
                         </div>
                         <div>
                             <div>
-                                <form>
+                                <div>
                                     <div class="flex justify-content-end gap-3 flex-wrap float-right mt-5 mb-3" >
                                         <div class="" >
                                             <InputFloatLabel inputFloatLabelProps={{
                                                 id: 'householdNumber',
                                                 text: translate(localeJson, 'name'),
+                                                onChange: (e) => {setSearchName(e.target.value)},
                                                 inputClass: "w-17rem lg:w-17rem md:w-20rem sm:w-14rem "
                                             }} parentClass={"w-full lg:w-22rem md:w-20rem sm:w-14rem"}
                                             />
@@ -167,15 +248,34 @@ export default function StaffManagementPage() {
                                                 buttonClass: "w-12 search-button",
                                                 text: translate(localeJson, "search_text"),
                                                 icon: "pi pi-search",
-                                                severity: "primary"
+                                                severity: "primary",
+                                                onClick: () => {
+                                                    setGetListPayload({...getListPayload, name: searchName})
+                                                }
                                             }} />
                                         </div>
                                     </div>
-                                </form>
+                                </div>
                             </div>
-                            <div>
-                            </div>
-                            <NormalTable paginator={"true"} paginatorLeft={true} showGridlines={"true"} columnStyle={{ textAlign: 'center' }} customActionsField="actions" value={staff} columns={staffs} />
+                            <div className='mt-3'>
+                                    <NormalTable
+                                        lazy
+                                        totalRecords={totalCount}
+                                        loading={tableLoading}
+                                        stripedRows={true}
+                                        className={"custom-table-cell"}
+                                        showGridlines={"true"}
+                                        value={list}
+                                        columns={columns}
+                                        filterDisplay="menu"
+                                        emptyMessage={translate(localeJson, "data_not_found")}
+                                        paginator={true}
+                                        first={getListPayload.filters.start}
+                                        rows={getListPayload.filters.limit}
+                                        paginatorLeft={true}
+                                        onPageHandler={(e) => onPaginationChange(e)}
+                                    />
+                                </div>
                         </div>
                     </div>
                 </div>
---
+++ src/services/staffmanagement.service.js
@@ -0,0 +1,168 @@
+import { downloadBase64File, timestampFile } from "@/helper";
+import axios from "@/utils/api";
+import toast from 'react-hot-toast';
+
+/* Identity and Access management (IAM) */
+export const StaffManagementService = {
+    importData: _importData,
+    exportData: _exportData,
+    getList: _getList,
+    create: _create,
+    update: _update,
+    delete: _delete,
+};
+
+/**
+ * Import place data
+ * @param {*} payload
+ * @param {*} callBackFun
+ */
+function _importData(payload, callBackFun) {
+    axios
+        .post("/admin/material/import", payload)
+        .then((response) => {
+            if (response && response.data) {
+                callBackFun(response.data);
+                if(response?.data?.success) {
+                    toast.success(response?.data?.message, {
+                        position: "top-right",
+                    });
+                } else {
+                    toast.error(response?.data?.message, {
+                        position: "top-right",
+                    });
+                }
+            }
+        })
+        .catch((error) => {
+            // Handle errors here
+            console.error("Error fetching data:", error);
+            toast.error(error?.response?.data?.message, {
+                position: "top-right",
+            });
+        });
+}
+
+/**
+ * Export place data
+ * @param {*} payload
+ * @param {*} callBackFun
+ */
+function _exportData(payload) {
+    axios
+        .post("/admin/material/export", payload)
+        .then((response) => {
+                if (response && response.data && response.data.result.filePath) {
+                    downloadBase64File(response.data.result.filePath, timestampFile("Material"));
+                    toast.success(response?.data?.message, {
+                        position: "top-right",
+                    });
+                }
+        })
+        .catch((error) => {
+            // Handle errors here
+            console.error("Error fetching data:", error);
+            toast.error(error?.response?.data?.message, {
+                position: "top-right",
+            });
+        });
+}
+
+/**
+ * Get place list
+ * @param {*} payload
+ * @param {*} callBackFun
+ */
+function _getList(payload, callBackFun) {
+    console.log(payload);
+    axios
+        .post("/admin/staff_management/list", payload)
+        .then((response) => {
+            // console.log(response);
+            if (response && response.data) {
+                callBackFun(response.data);
+            }
+        })
+        .catch((error) => {
+            // Handle errors here
+            console.error("Error fetching data:", error);
+            toast.error(error?.response?.data?.message, {
+                position: "top-right",
+            });
+        });
+}
+
+/**
+ * Create a new place
+ * @param {*} payload
+ * @param {*} callBackFun
+ */
+function _create(payload, callBackFun) {
+    axios
+        .post("/admin/material", payload)
+        .then((response) => {
+            if (response && response.data) {
+                callBackFun(response.data);
+                toast.success(response?.data?.message, {
+                    position: "top-right",
+                });
+            }
+        })
+        .catch((error) => {
+            // Handle errors here
+            console.error("Error fetching data:", error);
+            toast.error(error?.response?.data?.message, {
+                position: "top-right",
+            });
+        });
+}
+
+/**
+ * Update place by id
+ * @param {*} payload
+ * @param {*} callBackFun
+ */
+function _update(id, payload, callBackFun) {
+    axios
+        .put(`/admin/material/update`, payload)
+        .then((response) => {
+            if (response && response.data) {
+                callBackFun();
+                toast.success(response?.data?.message, {
+                    position: "top-right",
+                });
+            }
+        })
+        .catch((error) => {
+            // Handle errors here
+            console.error("Error fetching data:", error);
+            toast.error(error?.response?.data?.message, {
+                position: "top-right",
+            });
+        });
+}
+
+/**
+ * Get place details by id
+ * @param {*} id
+ * @param {*} callBackFun
+ */
+function _delete(id, callBackFun) {
+    axios
+        .delete(`/admin/material/delete`, {data: {"id": id}})
+        .then((response) => {
+            if (response && response.data) {
+                callBackFun(response.data);
+                toast.success(response?.data?.message, {
+                    position: "top-right",
+                });
+            }
+        })
+        .catch((error) => {
+            // Handle errors here
+            console.error("Error fetching data:", error);
+            toast.error(error?.response?.data?.message, {
+                position: "top-right",
+            });
+        });
+}
