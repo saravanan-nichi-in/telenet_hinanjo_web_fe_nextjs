b01454c2543e07d2551354d3f66296b9adc9657c
sandeepNIS <sandeep.r@nichi.com>
2023-10-25 15:14:16 +0530

    Fix:UI/UX / Date picker issue fix

--- src/components/date&time/dateTimeCalendarFloatLabel.js
+++ src/components/date&time/dateTimeCalendarFloatLabel.js
@@ -1,6 +1,11 @@
-import React, { useState } from 'react';
+import React, { useContext, useRef, useState } from 'react';
 import { Calendar } from 'primereact/calendar';
 import { addLocale } from 'primereact/api';
+
+import { Button } from '../button';
+import { LayoutContext } from "@/layout/context/layoutcontext";
+import { getValueByKeyRecursively as translate } from "@/helper";
+
 
 const DateTimeCalendarFloatLabel = (props) => {
     const {
@@ -30,8 +35,9 @@ const DateTimeCalendarFloatLabel = (props) => {
         text,
         ...restProps
     } = dateTimeFloatLabelProps;
-    const [date, setDate] = useState(props.date);
 
+    const [date, setDate] = useState(props.date);
+    const { localeJson } = useContext(LayoutContext);
     addLocale('en', {
         firstDayOfWeek: 0,
         dayNames: ['日曜日', '月曜日', '火曜日', '水曜日', '木曜日', '金曜日', '土曜日'],
@@ -47,40 +53,84 @@ const DateTimeCalendarFloatLabel = (props) => {
         clear: 'クリア'
     });
 
+    const calendarRef = useRef(null);
+
+    /**
+     *Function to close calendar modal
+     */
+    const closeCalendar = () => {
+        if (calendarRef.current) {
+            calendarRef.current.hide();
+        }
+    };
+
+    /**
+     * Function to update date
+     */
+    const handleUpdate = () => {
+        setDate(calendarRef.current.props.value);
+        closeCalendar();
+    };
+
     return (
         <div className='custom-align-label'>
-        <div className={`${parentClass} ${custom || 'custom_input'} p-float-label`}>
-            <Calendar className={` custom-dateCalendar ${dateTimeClass}`}
-                id={id}
-                showTime
-                inputId={inputId}
-                value={date}
-                name={name}
-                style={style}
-                onBlur={onBlur}
-                onChange={(e) => {
-                    setDate(e.value);
-                    if (onChange) {
-                        onChange(e);
+            <div className={`${parentClass} ${custom || 'custom_input'} p-float-label`}>
+                <Calendar className={` custom-dateCalendar ${dateTimeClass}`}
+                    id={id}
+                    showTime
+                    ref={calendarRef}
+                    inputId={inputId}
+                    value={date}
+                    name={name}
+                    style={style}
+                    onBlur={onBlur}
+                    onChange={(e) => {
+                        setDate(e.value);
+                        if (onChange) {
+                            onChange(e);
+                        }
                     }
-                }
-                }
-                dateFormat="yy年mm月dd日"
-                disabledDates={disabledDates}
-                disabledDays={disabledDays}
-                minDate={minDate}
-                maxDate={maxDate}
-                selectionMode={selectionMode || "single"}
-                readOnlyInput={readOnlyInput}
-                icon={icon || "pi pi-calendar"}
-                iconPos={iconPos || "right"}
-                showIcon={showIcon}
-                disabled={disabled}
-                placeholder={placeholder}
-                {...restProps}
-            />
-            <label htmlFor={inputId}>{text}</label>
-        </div>
+                    }
+                    dateFormat="yy年mm月dd日"
+                    disabledDates={disabledDates}
+                    disabledDays={disabledDays}
+                    minDate={minDate}
+                    maxDate={maxDate}
+                    selectionMode={selectionMode || "single"}
+                    readOnlyInput={readOnlyInput}
+                    icon={icon || "pi pi-calendar"}
+                    iconPos={iconPos || "right"}
+                    showIcon={showIcon}
+                    disabled={disabled}
+                    placeholder={placeholder}
+                    footerTemplate={() => (
+                        <>
+                            <hr />
+                            <div className="text-center">
+                                <Button buttonProps={{
+                                    buttonClass: "w-8rem",
+                                    type: "submit",
+                                    text: translate(localeJson, 'ok'),
+                                    severity: "primary",
+                                    onClick: handleUpdate
+                                }} parentClass={"inline"} />
+                                <Button buttonProps={{
+                                    buttonClass: "text-600 w-8rem",
+                                    bg: "bg-white",
+                                    hoverBg: "hover:surface-500 hover:text-white",
+                                    text: translate(localeJson, 'cancel'),
+                                    onClick: () => {
+                                        closeCalendar();
+                                    }
+                                }} parentClass={"inline pl-2"} />
+
+                            </div>
+                        </>
+                    )}
+                    {...restProps}
+                />
+                <label htmlFor={inputId}>{text}</label>
+            </div>
         </div>
     );
 }
