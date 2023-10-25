0566d4d0bdf78a38c5b51210d5582585b5dca01f
LikithKumarDK41 <99169124+LikithKumarDK41@users.noreply.github.com>
2023-10-25 16:31:18 +0530

    Update: Login / Toast

--- src/services/authn_authz.services.js
+++ src/services/authn_authz.services.js
@@ -40,10 +40,15 @@ function _login(key, values, callBackFun) {
                 if (response && response.data) {
                     staff.next(response.data);
                     callBackFun(response.data);
+                    toast.success(response?.data?.message, {
+                        position: "top-right",
+                    });
                 }
             })
             .catch((error) => {
-                console.error('Error fetching data:', error);
+                toast.error(error?.response?.data?.message, {
+                    position: "top-right",
+                });
             });
     }
 }
