09077352d043c69788b8acbaca6a61852aa32a24
HarishNichi <harish.devarayal@nichi.com>
2023-10-25 15:34:17 +0530

    map changes for info window

--- src/components/map/GoogleMapComponent.js
+++ src/components/map/GoogleMapComponent.js
@@ -1,69 +1,98 @@
 // GoogleMapComponent.jsx
-import React, { useState, useEffect } from 'react';
-import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
+import React, { useState, useEffect } from "react";
+import {
+  GoogleMap,
+  useJsApiLoader,
+  Marker,
+  InfoWindow,
+} from "@react-google-maps/api";
 
-const GoogleMapComponent = ({ initialPosition, height, searchResult }) => {
-    const containerStyle = {
-        width: '100%',
-        height: height || '100vh',
-    };
-    
-    const GOOGLE_API_KEY = process.env.NEXT_PUBLIC_GOOGLE_API_KEY;
-    const [center, setCenter] = useState();
-    const { isLoaded } = useJsApiLoader({
-        id: 'google-map-script',
-        googleMapsApiKey: GOOGLE_API_KEY
-    });
-    
-    const mapOptions = {
-        minZoom: 0, // Set your desired min zoom level
-        maxZoom: 4
-    };
 
-    useEffect(()=> {
-        setCenter(initialPosition)
-    },[]);
+const GoogleMapComponent = ({
+  initialPosition,
+  height,
+  searchResult,
+  popoverContent,
+  mapScale
+}) => {
+  const containerStyle = {
+    width: "100%",
+    height: height || "100vh",
+  };
 
-    useEffect(() => {
-        setCenter(initialPosition);
-    }, [initialPosition]);
-    useEffect(() => {
-        setCenter(searchResult);
-    }, [searchResult]);
+  const GOOGLE_API_KEY = process.env.NEXT_PUBLIC_GOOGLE_API_KEY;
+  const [center, setCenter] = useState(initialPosition);
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: GOOGLE_API_KEY,
+  });
+  const [selectedMarker, setSelectedMarker] = useState(null);
 
-    useEffect(() => {
-        if (searchResult) {
-            setCenter(searchResult);
-        }
-    }, [searchResult]);
+  const onMarkerClick = (marker) => {
+    setSelectedMarker(marker);
+  };
 
-    const onLoad = React.useCallback(async function callback(map) {
-        const bounds = new window.google.maps.LatLngBounds(center);
-        map.fitBounds(bounds);
-    }, [center]);
+  const mapOptions = {
+    minZoom: 0, // Set your desired min zoom level
+    maxZoom: 25,
+    zoom: mapScale||4,
+  };
 
-    const onUnmount = React.useCallback(function callback() {
-        setCenter(null);
-    }, []);
+  useEffect(() => {
+    setCenter(initialPosition);
+  }, [initialPosition]);
+  useEffect(() => {
+    setCenter(searchResult);
+  }, [searchResult]);
 
-    return isLoaded ? (
-        <>
-        <GoogleMap
-            mapContainerStyle={containerStyle}
-            onLoad={onLoad}
-            onUnmount={onUnmount}
-            zoom={1}
-            center={center}
-            options={mapOptions} // Add this line to use the mapOptions
-        >
-            { center && (
-               <Marker position={center} />
-            )
-            }
-        </GoogleMap>
-        </>
-    ) : <></>;
-    
+  useEffect(() => {
+    if (searchResult) {
+      setCenter(searchResult);
+    }
+  }, [searchResult]);
+
+  const onLoad = React.useCallback(
+    async function callback(map) {
+      const bounds = new window.google.maps.LatLngBounds(center);
+      map.fitBounds(bounds);
+    },
+    [center]
+  );
+
+  const onUnmount = React.useCallback(function callback() {
+    setCenter(null);
+  }, []);
+
+
+  
+
+  return isLoaded ? (
+    <>
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        onLoad={onLoad}
+        onUnmount={onUnmount}
+        center={center}
+        options={mapOptions}
+      >
+        {center && (
+          <Marker position={center} onClick={() => onMarkerClick(center)} />
+        )}
+        {selectedMarker && popoverContent && (
+            <>
+            <InfoWindow
+              position={center}
+              onCloseClick={() => setSelectedMarker(null)}
+            >
+              <div>{popoverContent}</div>
+            </InfoWindow>
+            </>
+        )}
+      </GoogleMap>
+    </>
+  ) : (
+    <></>
+  );
 };
 
 export default GoogleMapComponent;
--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -2,14 +2,17 @@ import { persistReducer } from 'redux-persist';
 import persistConfig from '@/redux/redux-persist-config';
 
 import authReducer from "./auth";
-import placeReducer from "./place"
+import placeReducer from "./place";
+import layoutReducer from './layout';
 
 const persistedAuthReducer = persistReducer(persistConfig, authReducer);
 const persistPlaceReducer = persistReducer(persistConfig, placeReducer);
+const persistLayoutReducer = persistReducer(persistConfig,layoutReducer);
 
 let rootReducer = {
   authReducer: persistedAuthReducer,
-  placeReducer: persistPlaceReducer
+  placeReducer: persistPlaceReducer,
+  layoutReducer: persistLayoutReducer
 };
 
 export default rootReducer;
