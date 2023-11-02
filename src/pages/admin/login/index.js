af852a14d773191c822abd852dc2cc7836f59c6c
LikithKumarDK41 <99169124+LikithKumarDK41@users.noreply.github.com>
2023-11-02 16:33:20 +0530

    UI/UX / Staff & Admin ( #2320 )

--- src/pages/admin/login/index.js
+++ src/pages/admin/login/index.js
@@ -30,7 +30,8 @@ const LoginPage = () => {
             }),
         password: Yup.string()
             .required(translate(localeJson, 'password_required'))
-            .min(8, translate(localeJson, 'password_atLeast_8_characters')),
+            .min(8, translate(localeJson, 'password_atLeast_8_characters'))
+            .max(15, translate(localeJson, 'password_max_15_characters')),
     });
 
     /* Services */
@@ -47,13 +48,12 @@ const LoginPage = () => {
         }
     };
 
-    const fetchSettingData =(res)=> {
-        if(res)
-        {
+    const fetchSettingData = (res) => {
+        if (res) {
             const data = res.data.model;
             dispatch(setLayout(data))
         }
-        
+
     }
 
     return (
