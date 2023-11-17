df4920bf94867c90ddf38d803e05e7f1c0722394
HarishNichi <harish.devarayal@nichi.com>
2023-11-17 12:24:39 +0530

    external screen integration

--- src/components/modal/externalModal.js
+++ src/components/modal/externalModal.js
@@ -3,41 +3,63 @@ import { Dialog } from "primereact/dialog";
 import { Formik } from "formik";
 import * as Yup from "yup";
 import { useRouter } from "next/router";
-
+import { gender_en, gender_jp } from "@/utils/constant";
 import Button from "../button/button";
-import { getValueByKeyRecursively as translate } from "@/helper";
+import {
+  getValueByKeyRecursively as translate,
+  getEnglishDateDisplayFormat,
+} from "@/helper";
 import { LayoutContext } from "@/layout/context/layoutcontext";
 import { ValidationError } from "../error";
-import { InputFloatLabel, InputIcon } from "../input";
+import { InputFloatLabel, InputIcon, InputNumberFloatLabel } from "../input";
+import { SelectFloatLabel } from "../dropdown";
+import { DateCalendarFloatLabel } from "@/components/date&time";
 
 export default function External(props) {
   const router = useRouter();
-  const { localeJson } = useContext(LayoutContext);
+  const { localeJson,locale } = useContext(LayoutContext);
+  // eslint-disable-next-line no-irregular-whitespace
+  const katakanaRegex = /^[\u30A1-\u30F6ー　]*$/;
+  const currentDate = new Date();
+  const minDOBDate = new Date();
+  minDOBDate.setFullYear(minDOBDate.getFullYear() - 120); // Set the maximum allowed birth date to 120 years ago
   const schema = Yup.object().shape({
     name: Yup.string()
-      .required(translate(localeJson, "questionnaire_name_is_required"))
-      .max(200, translate(localeJson, "questionnaire_name_max")),
-    dob: Yup.string().max(
-      255,
-      translate(localeJson, "questionnaire_remarks_is_max_required")
+      .required(translate(localeJson, "name_required"))
+      .max(200, translate(localeJson, "name_max"))
+      .matches(katakanaRegex, translate(localeJson, "name_katakana")),
+    dob: Yup.date().nullable().required(translate(localeJson, "dob_required"))
+      .min(minDOBDate, translate(localeJson, "min_dob"))
+      .max(currentDate, translate(localeJson, "max_dob")),
+    // Add other fields and validations as needed
+    age: Yup.number().required(translate(localeJson, "age_required")).max(
+      120,
+      translate(localeJson, "age_max")
     ),
-    age: Yup.string().max(
-      200,
-      translate(localeJson, "questionnaire_name_en_max")
-    ),
-    gender: Yup.string().max(
-      255,
-      translate(localeJson, "questionnaire_remarks_is_max_required")
-    ),
+    gender: Yup.string().required(translate(localeJson, "gender_required"))
   });
-  const { open, close, onSpecialCareEditSuccess, header, buttonText,setEvacueeValues } =
-    props && props;
-  const [initialValues] = useState({
-    name: "",
-    dob: "",
-    age: "",
-    gender: "",
-  });
+  const {
+    open,
+    close,
+    onSpecialCareEditSuccess,
+    header,
+    buttonText,
+    setEvacueeValues,
+    editObj,
+    registerModalAction,
+    evacuee,
+  } = props && props;
+
+  const initialValues =
+    registerModalAction == "edit"
+      ? editObj
+      : {
+        id: evacuee && evacuee.length > 0 ? evacuee.length + 1 : 1,
+        name: "",
+        dob: "",
+        age: "",
+        gender: null,
+      };
   return (
     <>
       <Formik
@@ -45,10 +67,10 @@ export default function External(props) {
         initialValues={initialValues}
         enableReinitialize
         onSubmit={(values, actions) => {
-            console.log(values)
-        setEvacueeValues(values)
+          values.dob = getEnglishDateDisplayFormat(values.dob);
+          setEvacueeValues(values);
           close();
-        //   actions.resetForm({ values: initialValues });
+          actions.resetForm({ values: initialValues });
         }}
       >
         {({
@@ -59,6 +81,7 @@ export default function External(props) {
           handleBlur,
           handleSubmit,
           resetForm,
+          setFieldValue,
         }) => (
           <div>
             <form onSubmit={handleSubmit}>
@@ -118,69 +141,89 @@ export default function External(props) {
                           inputClass:
                             "w-full lg:w-25rem md:w-23rem sm:w-21rem ",
                         }}
-                        parentClass={`${
-                          errors.name && touched.name && "p-invalid pb-1"
-                        }`}
+                        parentClass={`custom_input ${errors.name && touched.name && "p-invalid pb-1"
+                          }`}
                       />
                       <ValidationError
                         errorBlock={errors.name && touched.name && errors.name}
                       />
                     </div>
                     <div className="mt-5 ">
-                      <InputFloatLabel
-                        inputFloatLabelProps={{
-                          id: "dob",
+                      <DateCalendarFloatLabel
+                        date={values.dob}
+                        dateFloatLabelProps={{
                           name: "dob",
-                          value: values.dob,
-                          onChange: handleChange,
+                          dateClass: "w-full",
+                          spanText: "*",
+                          spanClass: "p-error",
+                          date: values.dob,
                           onBlur: handleBlur,
-                          text: translate(localeJson, "c_dob"),
-                          inputClass:
-                            "w-full lg:w-25rem md:w-23rem sm:w-21rem ",
+                          onChange: (evt) => {
+                            if (evt.target.value) {
+                              const birthDate = new Date(evt.target.value);
+                              const currentDate = new Date();
+                              const age =
+                                currentDate.getFullYear() -
+                                birthDate.getFullYear();
+                              setFieldValue("age", age);
+                              setFieldValue("dob", evt.target.value || "");
+                            }
+                          },
+                          placeholder: "yyyy-mm-dd",
+                          text: translate(localeJson, "dob"), // Add a label text specific to date
                         }}
-                        parentClass={`${
-                          errors.dob && touched.dob && "p-invalid pb-1"
-                        }`}
+                        parentClass={`custom_input ${errors.dob && touched.dob && "p-invalid pb-1"
+                          }`}
                       />
-                      <ValidationError
-                        errorBlock={errors.dob && touched.dob && errors.dob}
-                      />
+                      {errors.dob && (
+                        <ValidationError
+                          errorBlock={errors.dob && touched.dob && errors.dob}
+                        />
+                      )}
+
                     </div>
                     <div className="mt-5 ">
-                      <InputFloatLabel
-                        inputFloatLabelProps={{
+                      <InputNumberFloatLabel
+                        inputNumberFloatProps={{
                           id: "age",
                           name: "age",
                           value: values.age,
-                          onChange: handleChange,
+                          spanText: "*",
+                          spanClass: "p-error",
+                          disabled: true,
+                          onChange: (evt) => {
+                            setFieldValue("age", evt.value);
+                          },
+                          onValueChange: (evt) => {
+                            setFieldValue("age", evt.value);
+                          },
                           onBlur: handleBlur,
                           text: translate(localeJson, "c_age"),
-                          inputClass:
+                          inputNumberClass:
                             "w-full lg:w-25rem md:w-23rem sm:w-21rem ",
                         }}
-                        parentClass={`${
-                          errors.age && touched.age && "p-invalid pb-1"
-                        }`}
+                        parentClass={`custom_input ${errors.age && touched.age && "p-invalid pb-1"
+                          }`}
                       />
                       <ValidationError
                         errorBlock={errors.age && touched.age && errors.age}
                       />
                     </div>
                     <div className="mt-5 ">
-                      <InputFloatLabel
-                        inputFloatLabelProps={{
-                          id: "gender",
+                      <SelectFloatLabel
+                        selectFloatLabelProps={{
                           name: "gender",
                           value: values.gender,
+                          options: locale == "ja" ? gender_jp : gender_en,
+                          optionLabel: "name",
+                          selectClass:
+                            "w-full lg:w-25rem md:w-23rem sm:w-21rem ",
                           onChange: handleChange,
                           onBlur: handleBlur,
                           text: translate(localeJson, "c_gender"),
-                          inputClass:
-                            "w-full lg:w-25rem md:w-23rem sm:w-21rem ",
                         }}
-                        parentClass={`${
-                          errors.gender && touched.gender && "p-invalid pb-1"
-                        }`}
+                        parentClass={`custom_input ${errors.gender && touched.gender && "p-invalid pb-1"
+                          }`}
                       />
                       <ValidationError
                         errorBlock={
