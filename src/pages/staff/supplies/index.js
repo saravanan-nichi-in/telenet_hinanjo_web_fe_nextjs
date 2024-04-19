8359ef50a08046c22209dd0ce74969addbeb13e2
LikithKumarDK41 <likithkumar.dk@nichi.com>
2024-04-19 12:48:02 +0530

    Fix: Corrections

--- src/pages/staff/supplies/index.js
+++ src/pages/staff/supplies/index.js
@@ -180,7 +180,6 @@ export default function Supplies() {
           create(payload, isCreated);
           initialValues.comment = values.comment;
           initialValues.remarks = values.remarks;
-          setValues(initialValues);
           resetForm();
           setList([]);
           setTableLoading(true);
