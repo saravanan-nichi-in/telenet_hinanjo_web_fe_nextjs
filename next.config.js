9aa59349a0ba8b26f92c98f2433bb7dd0c63423c
LikithKumarDK41 <99169124+LikithKumarDK41@users.noreply.github.com>
2024-05-07 15:40:41 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,6 +7,11 @@ const nextConfig = {
     env: {
         customKey: process.env.NODE_ENV === 'development' ? 'my-value-dev' : 'my-value-prod',
     },
+    eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig
